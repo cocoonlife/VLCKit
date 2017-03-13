a56220186c2e69423e1229c0366204f7e79e1269
timwredwards <tim.edwards@cocoon.life>
2017-03-13 13:05:36 +0000

    Added DynamicMobileVLCKit project to workspace

---
+++ DynamicMobileVLCKit/DynamicMobileVLCKit.h
@@ -0,0 +1,19 @@
+//
+//  DynamicVLC.h
+//  DynamicVLC
+//
+//  Created by Tim Edwards on 09/03/2017.
+//  Copyright © 2017 Tim Edwards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicVLC.
+FOUNDATION_EXPORT double DynamicVLCVersionNumber;
+
+//! Project version string for DynamicVLC.
+FOUNDATION_EXPORT const unsigned char DynamicVLCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicVLC/PublicHeader.h>
+
+
